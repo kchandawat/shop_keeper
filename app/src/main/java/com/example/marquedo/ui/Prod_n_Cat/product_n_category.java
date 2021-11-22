fc34dae8955793768e57b8eac7955955c4e8c26f
zOrOjUrO <sgokulraj.cs19@rvce.edu.in>
2021-11-22 19:13:03 +0530

    -Converted Product & Category Activity to Fragment Implementation

--- app/src/main/java/com/example/marquedo/ui/Prod_n_Cat/product_n_category.java
+++ app/src/main/java/com/example/marquedo/ui/Prod_n_Cat/product_n_category.java
@@ -1,37 +1,44 @@
 package com.example.marquedo.ui.Prod_n_Cat;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.marquedo.R;
 import com.google.android.material.tabs.TabLayout;
 
-public class product_n_category extends AppCompatActivity {
+public class product_n_category extends Fragment {
 
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
     private HorizontalScrollView filters;
     private PnCFragmentAdapter adapter;
 
+    public static product_n_category newInstance() {
+        return new product_n_category();
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_products_n_category, container, false);
 
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager2 = view.findViewById(R.id.viewPager);
+        filters = view.findViewById(R.id.filter_view);
 
-        tabLayout = findViewById(R.id.tabLayout);
-        viewPager2 = findViewById(R.id.viewPager);
-        filters = findViewById(R.id.filter_view);
+        tabLayout.addTab(tabLayout.newTab().setText("Products"));
+        tabLayout.addTab(tabLayout.newTab().setText("Categories"));
 
-        tabLayout.addTab(tabLayout.newTab().setText("A"));
-        tabLayout.addTab(tabLayout.newTab().setText("B"));
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
         adapter = new PnCFragmentAdapter(fragmentManager, getLifecycle());
         viewPager2.setAdapter(adapter);
 
@@ -64,6 +71,7 @@ public class product_n_category extends AppCompatActivity {
             }
         });
 
+        return view;
     }
 
 }
