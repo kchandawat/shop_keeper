29e6e3f55a08b422ea868589f011dc78905e3166
Darshan <darshan010gowda@gmail.com>
2021-11-22 19:58:23 +0530

    linked screens: Orders, products, notifications yet to link: Home, dashboard

--- app/src/main/java/com/marquedo/marquedo/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/marquedo/marquedo/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,6 @@
 package com.marquedo.marquedo.ui.notifications;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.marquedo.marquedo.databinding.FragmentNotificationsBinding;
 
@@ -21,20 +24,12 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        View view = inflater.inflate(R.layout.notification_page, container, false);
+        RecyclerView rvOrders = (RecyclerView) view.findViewById(R.id.taskRecycler);
+        RecyclerAdapter adapter = new RecyclerAdapter();
+        rvOrders.setAdapter(adapter);
+        rvOrders.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        return view;
     }
 
     @Override
@@ -43,5 +38,52 @@ public class NotificationsFragment extends Fragment {
         binding = null;
     }
 
+    public class RecyclerAdapter extends
+            RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
 
+        // Provide a direct reference to each of the views within a data item
+        // Used to cache the views within the item layout for fast access
+        public  class ViewHolder extends RecyclerView.ViewHolder {
+            // Your holder should contain a member variable
+            // for any view that will be set as you render a row
+
+
+            // We also create a constructor that accepts the entire item row
+            // and does the view lookups to find each subview
+            public ViewHolder(View itemView) {
+                // Stores the itemView in a public final member variable that can be used
+                // to access the context from any ViewHolder instance.
+                super(itemView);
+
+            }
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            Context context = parent.getContext();
+            LayoutInflater inflater = LayoutInflater.from(context);
+
+            // Inflate the custom layout
+            View ordersView = inflater.inflate(R.layout.notification_tile, parent, false);
+
+            // Return a new holder instance
+            RecyclerAdapter.ViewHolder viewHolder = new RecyclerAdapter.ViewHolder(ordersView);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+
+
+
+        // Involves populating data into the item through holder
+
+    }
 }
