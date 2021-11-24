e1e5cc630ad8d2e3d989136584ce0ffea90863f7
Darshan <darshan010gowda@gmail.com>
2021-11-24 20:35:15 +0530

    improved class models for orders and enquiries page

--- app/src/main/java/com/example/marquedo/ui/Orders_Enquiries/Orders_n_Enquiry.java
+++ app/src/main/java/com/example/marquedo/ui/Orders_Enquiries/Orders_n_Enquiry.java
@@ -1,55 +1,52 @@
 package com.example.marquedo.ui.Orders_Enquiries;
 
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.example.marquedo.MyFragmentAdapter;
 import com.example.marquedo.R;
 import com.google.android.material.tabs.TabLayout;
 
-public class OrdersEnquiryFragment extends Fragment {
+public class Orders_n_Enquiry extends Fragment {
 
-    private OrdersEnquiryViewModel mViewModel;
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
-    private MyFragmentAdapter adapter;
+    private HorizontalScrollView filters;
+    private OrdersEnquiriesFragmentAdapter adapter;
 
-    public static OrdersEnquiryFragment newInstance() {
-        return new OrdersEnquiryFragment();
+    public static Orders_n_Enquiry newInstance() {
+        return new Orders_n_Enquiry();
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_order_enquiry_page, container, false);
-        TextView viewdetails = v.findViewById(R.id.tvViewDetails);
+        View view = inflater.inflate(R.layout.activity_order_enquiry_page, container, false);
 
-        tabLayout = v.findViewById(R.id.order_enquiry_tab);
-        viewPager2 = v.findViewById(R.id.view_pager_order_page);
+        tabLayout = view.findViewById(R.id.order_enquiry_tab);
+        viewPager2 = view.findViewById(R.id.view_pager_order_page);
+        filters = view.findViewById(R.id.Orders_Enquiries_filter);
 
         tabLayout.addTab(tabLayout.newTab().setText("Orders"));
         tabLayout.addTab(tabLayout.newTab().setText("Enquiries"));
 
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        adapter = new MyFragmentAdapter(fragmentManager, getLifecycle());
+        adapter = new OrdersEnquiriesFragmentAdapter(fragmentManager, getLifecycle());
         viewPager2.setAdapter(adapter);
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager2.setCurrentItem(tab.getPosition());
+
             }
 
             @Override
@@ -70,16 +67,7 @@ public class OrdersEnquiryFragment extends Fragment {
             }
         });
 
-
-
-        return v;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(OrdersEnquiryViewModel.class);
-        // TODO: Use the ViewModel
+        return view;
     }
 
 }
