ee42b5798661e77c8c6f200cdf8548bc463424a7
kchandawat <kchandawat@gmail.com>
2021-11-27 11:23:29 +0530

    Backend_frontend_merged

---
+++ app/src/main/java/com/marquedo/marquedo/preview_product.java
@@ -0,0 +1,17 @@
+package com.marquedo.marquedo;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class preview_product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_product_preview);
+        ImageButton close = findViewById(R.id.close_btn);
+        close.setOnClickListener(view -> finish());
+    }
+}
---
+++ app/src/main/java/com/marquedo/marquedo/ui/Orders_Enquiries/Enquiries/EnquiriesDataModel.java
@@ -0,0 +1,91 @@
+package com.marquedo.marquedo.ui.Orders_Enquiries.Enquiries;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class EnquiriesDataModel {
+
+    public String id;
+    public String date;
+    public String time;
+    public String status;
+    public String billTotal;
+    public String products;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBillTotal() {
+        return billTotal;
+    }
+
+    public void setBillTotal(String billTotal) {
+        this.billTotal = billTotal;
+    }
+
+    public String getProducts() {
+        return products;
+    }
+
+    public void setProducts(String products) {
+        this.products = products;
+    }
+
+    EnquiriesDataModel(JSONObject object){
+        try {
+            this.id = object.getString("orderID");
+            this.date = object.getString("date");
+            this.time = object.getString("time");
+            this.products = object.getString("product");
+            this.billTotal = object.getString("billtotal");
+            this.status = object.getString("status");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static ArrayList<EnquiriesDataModel> fromJson(JSONArray jsonObjects) {
+        ArrayList<EnquiriesDataModel> orders = new ArrayList<EnquiriesDataModel>();
+        for (int i = 0; i < jsonObjects.length(); i++) {
+            try {
+                orders.add(new EnquiriesDataModel(jsonObjects.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return orders;
+    }
+}
