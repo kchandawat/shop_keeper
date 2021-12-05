8b809525075fe279b0c4bb1822f4918664194874
zOrOjUrO <68921071+zOrOjUrO@users.noreply.github.com>
2021-12-05 22:53:17 +0530

    -Added Create Coupon Fragment -Changed Switch Style -Added Custom Radio Drawables

--- app/src/main/java/com/marquedo/marquedo/ui/dashboard/discounts/CreateCouponFragment.java
+++ app/src/main/java/com/marquedo/marquedo/ui/dashboard/discounts/CreateCouponFragment.java
@@ -12,10 +12,10 @@ import com.marquedo.marquedo.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DashLandingFragment#newInstance} factory method to
+ * Use the {@link CreateCouponFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DashLandingFragment extends Fragment {
+public class CreateCouponFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class DashLandingFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DashLandingFragment() {
+    public CreateCouponFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class DashLandingFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DashLandingFragment.
+     * @return A new instance of fragment CreateCouponFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DashLandingFragment newInstance(String param1, String param2) {
-        DashLandingFragment fragment = new DashLandingFragment();
+    public static CreateCouponFragment newInstance(String param1, String param2) {
+        CreateCouponFragment fragment = new CreateCouponFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class DashLandingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dash_landing, container, false);
+        return inflater.inflate(R.layout.fragment_create_coupon, container, false);
     }
 }
