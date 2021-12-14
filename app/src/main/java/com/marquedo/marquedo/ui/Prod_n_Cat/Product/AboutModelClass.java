85d24185b7e29b7edbb340f991921b8b5bffff50
Stuti Jain <mailtostuti41@gmail.com>
2021-12-14 16:47:54 +0530

    Product recycler view retrieval

---
+++ app/src/main/java/com/marquedo/marquedo/ui/Prod_n_Cat/Product/AboutModelClass.java
@@ -0,0 +1,27 @@
+package com.marquedo.marquedo.ui.Prod_n_Cat.Product;
+
+public class AboutModelClass
+{
+    String Category;
+
+
+    public AboutModelClass()
+    {
+
+    }
+
+    public AboutModelClass(String category)
+    {
+        Category = category;
+    }
+
+    public String getCategory()
+    {
+        return Category;
+    }
+
+    public void setCategory(String category)
+    {
+        Category = category;
+    }
+}
