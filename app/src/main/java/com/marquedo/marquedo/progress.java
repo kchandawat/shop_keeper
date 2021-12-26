48add5399d6bd72f2d45cd1a9934369840a8188d
zOrOjUrO <68921071+zOrOjUrO@users.noreply.github.com>
2021-12-26 23:56:32 +0530

    -Added Services Fragment and Corresponding Data Model and Adapters -Added Services to the Bottom Navigation -Linked Add Service button to AddProductNServiceActivity -Setup Intent Extras and intent extra handle to direct to the specific Fragment in PnS Fragments

--- app/src/main/java/com/marquedo/marquedo/progress.java
+++ app/src/main/java/com/marquedo/marquedo/progress.java
@@ -27,7 +27,10 @@ public class progress extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_orders_enquiries, R.id.navigation_products_categories, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_orders_enquiries,
+                R.id.navigation_products_categories, R.id.navigation_dashboard,
+                //R.id.navigation_notifications,
+                R.id.navigation_services)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_progress);
         binding.navView.setItemIconTintList(null);
